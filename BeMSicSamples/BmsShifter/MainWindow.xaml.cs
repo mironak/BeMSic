8b6477c30ce2274e5d089ccb6ca6bbcaa00129f4
mironak <iymits@gmail.com>
2023-05-14 21:28:53 +0900

    BmsShifterから不要機能削除

--- BeMSicSamples/BmsShifter/MainWindow.xaml.cs
+++ BeMSicSamples/BmsShifter/MainWindow.xaml.cs
@@ -29,60 +29,17 @@ namespace BmsShifter
                 var bmsText = File.ReadAllText(TargetBmsPathTextBox.Text, Encoding.GetEncoding("shift_jis"));
                 var targetBmsConverter = new BmsConverter(bmsText);
 
-                // Output wav added bms
-                if (WavBmsPathLabel.Text != String.Empty)
-                {
-                    var wavBmsText = File.ReadAllText(WavBmsPathLabel.Text, Encoding.GetEncoding("shift_jis"));
-                    var wavBmsConverter = new BmsConverter(wavBmsText);
-                    wavBmsConverter.DeleteUnusedWav().ArrangeWav();
-
-                    wavBmsConverter.WavMarge(targetBmsConverter.Bms);
-                    File.WriteAllText(GetWavAddedOutputName(WavBmsPathLabel.Text), wavBmsConverter.Bms);
-                }
-
                 // Output wav shifted bms
                 var offset = new WavDefinition(WavStartTextBox.Text);
                 var shift = int.Parse(BgmShiftSizeTextBox.Text);
                 targetBmsConverter.Offset(offset.Num - 1).Shift(shift);
                 File.WriteAllText(_targetOutputName, targetBmsConverter.Bms);
-                WavBmsPathLabel.Text = String.Empty;
 
                 MessageBox.Show("Completed.");
             }
-            //catch (ArgumentOutOfRangeException)
-            //{
-            //    MessageBox.Show("Input is out of range.");
-            //}
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-                MessageBox.Show("Please read the bms file.");
-            }
-        }
-
-        /// <summary>
-        /// #WAV Startボタン処理
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void WavStartFromBmsButton_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new OpenFileDialog
-            {
-                Filter = "bms file|*.bms;*.bme;*.bml;*.pms|All file|*.*"
-            };
-
-            try
-            {
-                if (dialog.ShowDialog() == false)
-                {
-                    return;
-                }
-
-                WavStart_OpenBms(dialog.FileName);
-            }
-            catch
-            {
                 MessageBox.Show("Please read the bms file.");
             }
         }
@@ -105,22 +62,6 @@ namespace BmsShifter
             }
 
             TargetBmsPath_OpenBms(loadDialog.FileName);
-        }
-
-        /// <summary>
-        /// Windowへのドロップ時処理
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void WavStart_Drop(object sender, DragEventArgs e)
-        {
-            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                return;
-            }
-
-            var fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
-            WavStart_OpenBms(fileNames[0]);
         }
 
         /// <summary>
@@ -156,22 +97,6 @@ namespace BmsShifter
             }
 
             e.Handled = true;
-        }
-
-        /// <summary>
-        /// bmsファイルを開く
-        /// </summary>
-        /// <param name="fileName"></param>
-        private void WavStart_OpenBms(string fileName)
-        {
-            WavBmsPathLabel.Text = fileName;
-            var bmsText = File.ReadAllText(fileName, Encoding.GetEncoding("shift_jis"));
-            var wavBmsConverter = new BmsConverter(bmsText);
-            wavBmsConverter.DeleteUnusedWav().ArrangeWav();
-            var replace = new BmsDefinitionReplace(wavBmsConverter.Bms);
-            WavDefinitions wavs = replace.GetUsedWavList();
-            var wavStart = new WavDefinition(wavs.GetMax().Num + 1);
-            WavStartTextBox.Text = wavStart.ZZ;
         }
 
         /// <summary>
